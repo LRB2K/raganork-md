67d676ba2804a912d23131b2f96534d2b6f08662
LRB2K <94140435+LRB2K@users.noreply.github.com>
2024-06-10 16:13:47 +0530

    Update tag.js
    Number change emoji

--- plugins/tag.js
+++ plugins/tag.js
@@ -8,6 +8,8 @@ const {getString} = require('./misc/lang');
 const {readFileSync} = require('fs');
 const {saveMessage} = require('./misc/saveMessage');
 const Lang = getString('group');
+const emojis = ['😀', '😃', '😄', '😁', '😆', '😅', '😂', '🤣', '😊', '😇']; // ইমোজি লিস্ট
+
 Module({pattern: 'tag ?(.*)',use: 'group', fromMe: true, desc: Lang.TAGALL_DESC}, (async (message, match) => {
 if (match[1] === "all" || !message.reply_message) return;
 var target = message.jid
@@ -23,13 +25,13 @@ if (match[1].includes('admin')){
 group.participants.map(async(user) => {
 jids.push(user.id.replace('c.us', 's.whatsapp.net'));});
 }
-await message.forwardMessage(target,message.quoted,{detectLinks: true,contextInfo: {mentionedJid: jids}});
+await message.forwardMessage(target,message.quoted,{detectLinks: true,contextInfo: {mentionedJid: jids, mentionedEmoji: emojis.slice(0, jids.length)}});
 }))
 Module({pattern: 'forward ?(.*)',use: 'utility', fromMe: true, desc: "Forwards message"}, (async (message, match) => {
     if (!message.reply_message) return await message.sendReply("*Reply to a message*\n*Ex: .forward jid jid ...*")
     let Jids = [...match[1]?.match(/[0-9]+(-[0-9]+|)(@g.us|@s.whatsapp.net)/g)] || [message.jid]
         for (let jid of Jids) {
-      await message.forwardMessage(jid, message.quoted,{detectLinks: true});
+      await message.forwardMessage(jid, message.quoted,{detectLinks: true, contextInfo: {mentionedEmoji: emojis.slice(0, Jids.length)}});
     }
 }));
 Module({pattern: 'send ?(.*)',use: 'utility', fromMe: true, desc: "Sends message"}, (async (message, match) => {
@@ -37,8 +39,7 @@ Module({pattern: 'send ?(.*)',use: 'utility', fromMe: true, desc: "Sends message
      if (!match[1]) match[1] = message.jid
     let Jids = [...match[1]?.match(/[0-9]+(-[0-9]+|)(@g.us|@s.whatsapp.net)/g)] || [message.jid];
         for (let jid of Jids) {
-     await message.forwardMessage(jid, message.quoted,{contextInfo:{isForwarded: false},detectLinks: true});
+     await message.forwardMessage(jid, message.quoted,{contextInfo:{isForwarded: false, mentionedEmoji: emojis.slice(0, Jids.length)}, detectLinks: true});
     await new Promise((r) => setTimeout(r, 5000))
         }
 }));
-        
